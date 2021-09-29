73c6f01cf58d4c419c09cfc7fba1ef54c2d428fa
SammyStax <57230694+Samuel5555@users.noreply.github.com>
2021-09-29 01:55:56 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
 import firebase from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import "firebase/firestore";
 
 // Your web app's Firebase configuration
-let firebaseConfig = {
-	apiKey: "AIzaSyBmbSg2xXXo4fKdc_zfBrtK-ZHDFOk1cV0",
-	authDomain: "sveltefire-test-b7129.firebaseapp.com",
-	projectId: "sveltefire-test-b7129",
-	storageBucket: "sveltefire-test-b7129.appspot.com",
-	messagingSenderId: "1022611496294",
-	appId: "1:1022611496294:web:8ca2ac4ae2f9baaa22fca7",
-	measurementId: "G-B95TGXN9LB",
+const firebaseConfig = {
+  apiKey: "AIzaSyBf5JMNiZxVJIGTPKGYeRn4p406qVGpbFM",
+  authDomain: "davidibiyeomie2021.firebaseapp.com",
+  projectId: "davidibiyeomie2021",
+  storageBucket: "davidibiyeomie2021.appspot.com",
+  messagingSenderId: "36468876993",
+  appId: "1:36468876993:web:f451736638327d93519640",
+  measurementId: "G-NB1ZRYN8PE"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const database = firebase.firestore();
